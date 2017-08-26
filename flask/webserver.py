b3222dbcfd06896b76d849b02941a62d4da461fd
Mingxun <mwang87@gmail.com>
2017-08-26 09:47:36 -0700

    working initial version of emporer wrapper

--- flask/webserver.py
+++ flask/webserver.py
@@ -4,9 +4,9 @@ import urllib
 import requests
 import tempfile
 import os
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/emperor_required_resources', static_folder="./static/emperor_required_resources")
 
-
+SCRATCH_FOLDER = "./scratch"
  # beta_diversity.py -i myexample_bucket_table.biom -m gower -o  myexample_bucket_table_gower
  # validate_mapping_file.py -m mymapping_fileV02.txt -o validate_mapping_file_output
  # less validate_mapping_file_output/mymapping_fileV02.log
@@ -16,23 +16,47 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     try:
-        #metadata_url = request.args["metadata"]
+        metadata_url = request.args["metadata"]
         biom_url = request.args["biom"]
     except:
         return "ERROR"
 
 
-    #temporary_metadata_file = tempfile.NamedTemporaryFile(delete=False)
-    #temporary_metadata_file.close()
-    #urllib.urlretrieve(metadata_url, temporary_metadata_file.name)
+    """File Retrievel"""
+    temporary_metadata_file = tempfile.NamedTemporaryFile(delete=False)
+    temporary_metadata_file.close()
+    urllib.urlretrieve(metadata_url, temporary_metadata_file.name)
 
     temporary_biom_file = tempfile.NamedTemporaryFile(delete=False)
     temporary_biom_file.close()
     urllib.urlretrieve(biom_url, temporary_biom_file.name)
 
-    temporary_betadiversity = tempfile.NamedTemporaryFile(delete=False)
-    temporary_betadiversity.close()
-    cmd = "beta_diversity.py -i %s -m gower -o %s" % (temporary_biom_file.name, temporary_betadiversity.name)
+    """Input file validation"""
+    cmd = "validate_mapping_file.py -m %s -o validate_mapping_file_output"
+
+
+    """Beta Diversity"""
+    temporary_betadiversity = os.path.join(SCRATCH_FOLDER, "beta_diversity")
+    beta_diversity_output_filename = os.path.join(temporary_betadiversity, "gower_%s.txt" % (os.path.basename(temporary_biom_file.name)))
+    cmd = "beta_diversity.py -i %s -m gower -o %s" % (temporary_biom_file.name, temporary_betadiversity)
+    print(cmd)
+    os.system(cmd)
+
+    """Principal Coordinates Analysis"""
+    temporary_pcoa_file = tempfile.NamedTemporaryFile(delete=False)
+    temporary_pcoa_file.close()
+    cmd = "principal_coordinates.py -i %s -o %s" % (beta_diversity_output_filename, temporary_pcoa_file.name)
+    print(cmd)
+    os.system(cmd)
+
+    """Make Emprorer"""
+    cmd = "make_emperor.py -i %s -m %s -o %s" % (temporary_pcoa_file.name, temporary_metadata_file.name, SCRATCH_FOLDER)
+    print(cmd)
+    os.system(cmd)
+
+    path_to_emporer_html = os.path.join(SCRATCH_FOLDER, "index.html")
+    return open(path_to_emporer_html).read()
+
 
     #Cleaning up temporary files
     #os.unlink(temporary_metadata_file.name)
