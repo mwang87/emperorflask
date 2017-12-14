84e277552d162a2b006fda34f6c9e2c1f44923cb
Mingxun Wang <mwang87@gmail.com>
2017-12-14 15:36:47 -0800

    updating docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,2 @@
+#docker run -it -p 5001:5000 -v $(pwd)/../flask:/server gnpsqiime /server/run_prod.sh
 docker run -it -p 5001:5000 -v $(pwd)/../flask:/server gnpsqiime bash
-#docker run -it -p 5001:5000 -v $(pwd)/../flask:/server gnpsqiime "gunicorn webserver:app -b 0.0.0.0:5000 --threads 4"
