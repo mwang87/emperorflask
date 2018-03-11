926f9ced631d01f1d5279dcba8ce0ceb5649b208
Mingxun Wang <mwang87@gmail.com>
2018-03-11 16:37:49 -0700

    updating docker for emperor

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,2 @@
-#docker run -it -p 5001:5000 -v $(pwd)/../flask:/server gnpsqiime /server/run_prod.sh
-docker run -it -p 5001:5000 -v $(pwd)/../flask:/server gnpsqiime bash
+docker run -d -p 5005:5000 -v $(pwd)/../flask:/server gnpsqiime /server/run_prod.sh
+#docker run -it -p 5005:5000 -v $(pwd)/../flask:/server gnpsqiime bash
--- flask/run_prod.sh
+++ flask/run_prod.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
+cd /server
 gunicorn webserver:app -b 0.0.0.0:5000 --threads 4 --timeout 3600 
