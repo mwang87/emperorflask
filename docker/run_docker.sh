fabe70cadc530b7d77289945bb1131dad28d7512
Mingxun Wang <mwang87@gmail.com>
2018-04-08 22:39:03 -0700

    naming docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,3 @@
-docker run -d -p 5005:5000 -v $(pwd)/../flask:/server gnpsqiime /server/run_prod.sh
-#docker run -it -p 5005:5000 -v $(pwd)/../flask:/server gnpsqiime bash
+docker rm gnpsqiime
+docker run -d -p 5005:5000 -v $(pwd)/../flask:/server --name gnpsqiime  gnpsqiime /server/run_prod.sh
+#docker run -it -p 5005:5000 -v $(pwd)/../flask:/server --name gnpsqiime gnpsqiime bash
